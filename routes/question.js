d98f47afca9ba9c472aba23372f4b69a9eab2774
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-31 15:13:42 -0500

    get and question/next working

--- routes/question.js
+++ routes/question.js
@@ -10,89 +10,110 @@ const router = express.Router();
 const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
 router.use(jwtAuth);
 
-let currentHead;
+let currentHead = 0;
 
 router
   .get( '/', (req, res, next) => {
     User.findById(req.user.id)
       .then((user) => {
         //on click of the next button, change the currentQUestion
-        const { word, answer, id } = user.questionData[user.head]; 
-        currentHead = user.head+1;
+        const { word, answer, id } = user.questionData[user.head];  //head is defaulted to zero
         // { question: { word, id}}
         //res.json(user);
         res.json({word, answer, id, head: user.head }); 
         //after confirming the answer matches on the client side, do a put request to change the m, we need the question id to be able to find the question id on the server
     
       })
-      .catch(next);
+      .catch(err => next(err));
   });
 
-/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+// router
+//   .get( '/next', (req, res, next) => {
+//     User.findById(req.user.id)
+//       .then((user) => {
+//         const { word, answer, id } = user.questionData[user.head+1]; 
+//         user.head++;
+//         res.json({word, answer, id, head: user.head }); 
+//         //after confirming the answer matches on the client side, do a put request to change the m, we need the question id to be able to find the question id on the server
+    
+//       })
+//       .catch(err => next(err));
+//   });
+
+// /* ========== PUT/UPDATE A SINGLE ITEM ========== */
+// router
+//   .put('/next',(req, res, next) => {
+//     //const { id } = req.params;
+//     // const { name } = req.body;
+//     //const userId = req.user.id;
+    
+//     User.findById()
+//       .then(result => {
+//         res.json(result);
+//       })
+//       .catch(err => {
+//         next(err);
+//       });
+//     // } 
+//   });
+
+// /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router
-  .put('/:userId',(req, res, next) => {
-    const { userId } = req.params;
+  .put('/:userid',(req, res, next) => {
+    const { userid } = req.params;
     //console.log('REQBODY',req.body);
-    const { word, id, userinput, answer } = req.body;
+    //const { word, id } = req.body;
     //const userId = req.user.id;
 
-    /***** Never trust users - validate input *****/
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-      const err = new Error('The `id` is not valid');
-      err.status = 400;
-      return next(err);
-    }
+    // /***** Never trust users - validate input *****/
+    // if (!mongoose.Types.ObjectId.isValid(userid)) {
+    //   const err = new Error('The `id` is not valid');
+    //   err.status = 400;
+    //   return next(err);
+    // }
 
-    if (!word) {
-      const err = new Error('Missing `word` in request body');
-      err.status = 400;
-      return next(err);
-    }
+    // if (!word) {
+    //   const err = new Error('Missing `word` in request body');
+    //   err.status = 400;
+    //   return next(err);
+    // }
 
-    if (!id && !mongoose.Types.ObjectId.isValid(id)) {
-      const err = new Error('Missing question `id` in request body');
-      err.status = 400;
-      return next(err);
-    }
+    // if (!id && !mongoose.Types.ObjectId.isValid(id)) {
+    //   const err = new Error('Missing question `id` in request body');
+    //   err.status = 400;
+    //   return next(err);
+    // }
 
-    if (!answer) {
-      const err = new Error('Missing `answer` in request body');
-      err.status = 400;
-      return next(err);
-    }
+    // if (!answer) {
+    //   const err = new Error('Missing `answer` in request body');
+    //   err.status = 400;
+    //   return next(err);
+    // }
 
-    if (!userinput) {
-      const err = new Error('Missing `userinput` in request body');
-      err.status = 400;
-      return next(err);
-    }
+    // if (!userinput) {
+    //   const err = new Error('Missing `userinput` in request body');
+    //   err.status = 400;
+    //   return next(err);
+    // }
 
-    const nextQuestion = {
-      m: 2,
-      word: 'saya',
-      answer: 'happy',
-      next: 2,
+    let changeQ = {
+      head: currentHead+1
     };
 
     //if the answer is correct: 
-    if( userinput === answer ){
-      Question.findOneAndUpdate(userId, nextQuestion)
-        .then(result => {
-        // const changeM = {
-        //   m: result.m*2,
-        //   word: result.word,
-        //   answer: result.answer,
-        //   next: currentHead,
-        //   id: result.id
-        // };
-        // currentHead+1;
+    //if( userinput === answer ){
+    User.findOneAndUpdate(userid, changeQ, { new: true})
+      .then(result => {
         //res.json(changeM);
-          res.json(result);
-        })
-        .catch(err => {
-          next(err);
-        });
-    } 
+        res.json(result);
+      })
+      .then(result => {
+        return currentHead++;
+      })
+      .catch(err => {
+        next(err);
+      });
+    // } 
   });
 
 module.exports = router;
