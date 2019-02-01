41b60c62b2f83efffac03686cb1d215ae5a27828
kronicle114 <trisha.aguinaldo@gmail.com>
2019-02-01 08:38:16 -0500

    postman put and get working

--- routes/question.js
+++ routes/question.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const express = require('express');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const passport = require('passport');
 const User = require('../models/user');
-const Question = require('../models/question');
+//const Question = require('../models/question');
 const router = express.Router();
 
 const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
@@ -17,47 +17,17 @@ router
     User.findById(req.user.id)
       .then((user) => {
         //on click of the next button, change the currentQUestion
-        const { word, answer, id } = user.questionData[user.head];  //head is defaulted to zero
+        const { word, answer, id } = user.questionData[currentHead];  //head is defaulted to zero
         // { question: { word, id}}
         //res.json(user);
-        res.json({word, answer, id, head: user.head }); 
+        res.json({word, answer, id, head: currentHead }); 
         //after confirming the answer matches on the client side, do a put request to change the m, we need the question id to be able to find the question id on the server
     
       })
       .catch(err => next(err));
   });
 
-// router
-//   .get( '/next', (req, res, next) => {
-//     User.findById(req.user.id)
-//       .then((user) => {
-//         const { word, answer, id } = user.questionData[user.head+1]; 
-//         user.head++;
-//         res.json({word, answer, id, head: user.head }); 
-//         //after confirming the answer matches on the client side, do a put request to change the m, we need the question id to be able to find the question id on the server
-    
-//       })
-//       .catch(err => next(err));
-//   });
-
-// /* ========== PUT/UPDATE A SINGLE ITEM ========== */
-// router
-//   .put('/next',(req, res, next) => {
-//     //const { id } = req.params;
-//     // const { name } = req.body;
-//     //const userId = req.user.id;
-    
-//     User.findById()
-//       .then(result => {
-//         res.json(result);
-//       })
-//       .catch(err => {
-//         next(err);
-//       });
-//     // } 
-//   });
-
-// /* ========== PUT/UPDATE A SINGLE ITEM ========== */
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router
   .put('/:userid',(req, res, next) => {
     const { userid } = req.params;
@@ -66,20 +36,16 @@ router
       head: currentHead+1
     };
 
-    //if the answer is correct: 
-    //if( userinput === answer ){
     User.findOneAndUpdate(userid, changeQ, { new: true})
       .then(result => {
-        //res.json(changeM);
         res.json(result.questionData[currentHead]);
       })
-      .then(result => {
+      .then(() => {
         return currentHead++;
       })
       .catch(err => {
         next(err);
       });
-    // } 
   });
 
 module.exports = router;
